85e252e9fdf5569d0be1e6757b0cc75ab8abd134
Paul-Sender <PaulSender99@gmail.com>
2019-03-15 15:02:24 -0400

    Converting yards, feet, and inches to total inches

---
+++ ConvertInchesToYards.py
@@ -0,0 +1,9 @@
+#Program that takes in a user input of inches and converts it into Yards, Feet, and remaining inches
+#3/11/2019
+inputyards = 3
+inputfeet = 12
+inputinches = 100
+yardsinches = inputyards * 36
+feetinches = inputfeet * 12
+totalinches = yardsinches + feetinches + inputinches
+print(str(totalinches))
